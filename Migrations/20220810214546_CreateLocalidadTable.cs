2bd00dc50a5bd60eb77983182cf07c53526ff4da
Camilo Calderón Tapia <cristian.calderontapia@idartes.gov.co>
2022-08-10 17:38:21 -0500

    Migraciones

---
+++ Migrations/20220810214546_CreateLocalidadTable.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SaludMigrantes.Migrations
+{
+    public partial class CreateLocalidadTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LOCALIDAD",
+                columns: table => new
+                {
+                    ID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NOMBRE = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LOCALIDAD", x => x.ID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "LOCALIDAD",
+                columns: new[] { "ID", "NOMBRE" },
+                values: new object[,]
+                {
+                    { 1L, "01. USAQUÉN" },
+                    { 2L, "02. CHAPINERO" },
+                    { 3L, "03. SANTA FE" },
+                    { 4L, "04. SAN CRISTOBAL" },
+                    { 5L, "05. USME" },
+                    { 6L, "06. TUNJUELITO" },
+                    { 7L, "07. BOSA" },
+                    { 8L, "08. KENNEDY" },
+                    { 9L, "09. FONTIBÓN" },
+                    { 10L, "10. ENGATIVÁ" },
+                    { 11L, "11. SUBA" },
+                    { 12L, "12. BARRIOS UNIDOS" },
+                    { 13L, "13. TEUSAQUILLO" },
+                    { 14L, "14. MÁRTIRES" },
+                    { 15L, "15. ANTONIO NARIÑO" },
+                    { 16L, "16. PUENTE ARANDA" },
+                    { 17L, "17. CANDELARIA" },
+                    { 18L, "18. RAFAEL URIBE" },
+                    { 19L, "19. CIUDAD BOLIVAR" },
+                    { 29L, "20. SUMAPAZ" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LOCALIDAD");
+        }
+    }
+}
---
+++ Migrations/20220810221833_FillDeclaracionMigrantesTable.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SaludMigrantes.Migrations
+{
+    public partial class FillDeclaracionMigrantesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+            @"
+                declare @i int  = (SELECT ISNULL(MAX(ID),0) + 1 FROM DECLARACIONES_MIGRANTES)
+                update DECLARACIONES_MIGRANTES
+                set ID  = @i , @i = @i + 1
+                where ID is null                
+            ");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "ID",
+                table: "DECLARACIONES_MIGRANTES",
+                nullable: false
+            );
+            
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_DECLARACION",
+                table: "DECLARACIONES_MIGRANTES",
+                column: "ID"
+            );
+
+
+            migrationBuilder.Sql(
+            @"
+                update DECLARACIONES_MIGRANTES
+                set FECHA_VIGENCIA = DATEADD(MONTH , 4, FECHA_DECLARA)
+                where FECHA_VIGENCIA is null              
+            ");                 
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "FECHA_VIGENCIA",
+                table: "DECLARACIONES_MIGRANTES",
+                type: "Date",
+                nullable: false
+            );            
+                       
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_DECLARACION",
+                table: "DECLARACIONES_MIGRANTES"                
+            );
+
+            migrationBuilder.AlterColumn<long>(
+                name: "ID",
+                table: "DECLARACIONES_MIGRANTES",
+                nullable: true
+            );            
+                        
+            migrationBuilder.Sql(
+            @"
+                update DECLARACIONES_MIGRANTES
+                set ID  = null
+                where ID is not null                
+            ");
+
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "FECHA_VIGENCIA",
+                table: "DECLARACIONES_MIGRANTES",
+                type: "Date",
+                nullable: true
+            );             
+
+            migrationBuilder.Sql(
+            @"
+                update DECLARACIONES_MIGRANTES
+                set FECHA_VIGENCIA = null
+                where FECHA_VIGENCIA is not null              
+            ");             
+        }
+    }
+}
